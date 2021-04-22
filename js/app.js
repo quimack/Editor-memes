2623471e09e3089686add1ce96e5920a0db93e60
quimack <75865290+quimack@users.noreply.github.com>
2021-04-22 17:51:02 -0300

    mejoré variables de colores, eliminé codigo demás, emprolijé modo oscuro

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
+const body = document.querySelector('#body');
 const modoClaro = document.querySelector('#modoClaro');
 const modoOscuro = document.querySelector('#modoOscuro');
+const botonImagen = document.querySelector('#botonImagen');
+const botonTexto = document.querySelector('#botonTexto');
+const cerrarPanelImagen = document.querySelector('#cerrarPanelImagen');
+const panelImagen = document.querySelector('#panelImagen');
+const panelMeme = document.querySelector('#panelMeme');
+const aside = document.querySelector('#aside');
 
+//IR A MODO OSCURO 
 modoClaro.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+   body.classList.toggle('dark');
+   body.classList.toggle('light');
     modoClaro.classList.toggle('oculto');
     modoOscuro.classList.toggle('oculto');
 });
-
-console.log(modoOscuro.classList.contains('oculto'));
-
+//IR A MODO CLARO 
 modoOscuro.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
     modoClaro.classList.toggle('oculto');
     modoOscuro.classList.toggle('oculto');
 });
+
+//ABRIR PANEL IMAGEN
+botonImagen.addEventListener('click', () => {
+    aside.classList.remove('oculto');
+    panelMeme.classList.add('oculto');
+});
+
+//CERRAR PANEL IMAGEN
+cerrarPanelImagen.addEventListener('click', () => {
+    aside.classList.add('oculto');
+    panelMeme.classList.remove('oculto');
+});
+
+
+
 
 
 
