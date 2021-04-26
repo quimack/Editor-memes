df2ed9ff1a57ea9b47bd35b05d45de29aa4b0c14
quimack <75865290+quimack@users.noreply.github.com>
2021-04-26 17:08:13 -0300

    intenté resolver el problema de los paneles

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const modoClaro = document.querySelector('#modoClaro');
 const modoOscuro = document.querySelector('#modoOscuro');
 const botonImagen = document.querySelector('#botonImagen');
 const botonTexto = document.querySelector('#botonTexto');
+const panelTexto = document.querySelector('#panelTexto');
 const cerrarPanelImagen = document.querySelector('#cerrarPanelImagen');
 const panelImagen = document.querySelector('#panelImagen');
 const panelMeme = document.querySelector('#panelMeme');
@@ -25,7 +26,7 @@ modoOscuro.addEventListener('click', () => {
 
 //ABRIR PANEL IMAGEN
 botonImagen.addEventListener('click', () => {
-    aside.classList.remove('oculto');
+    aside.classList.remove('oculto')
     panelMeme.classList.add('oculto');
 });
 
@@ -34,6 +35,15 @@ cerrarPanelImagen.addEventListener('click', () => {
     aside.classList.add('oculto');
     panelMeme.classList.remove('oculto');
 });
+
+//ABRIR PANEL TEXTO
+
+botonTexto.addEventListener('click', () => {
+    panelMeme.classList.add('oculto');
+    aside.classList.remove('oculto');
+    panelTexto.classList.remove('oculto');
+    panelImagen.classList.add('oculto');
+})
 
 
 
