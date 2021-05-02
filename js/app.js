0b8de109e4bfd408135de26b422a7a9626e6b025
quimack <75865290+quimack@users.noreply.github.com>
2021-05-02 14:59:12 -0300

    terminé HTML del panel de texto

--- js/app.js
+++ js/app.js
@@ -8,6 +8,14 @@ const cerrarPanelImagen = document.querySelector('#cerrarPanelImagen');
 const panelImagen = document.querySelector('#panelImagen');
 const panelMeme = document.querySelector('#panelMeme');
 const aside = document.querySelector('#aside');
+const urlImg = document.querySelector('#urlImg');
+const imgMeme = document.querySelector('#imgMeme');
+const brillo = document.querySelector('#controlBrillo');
+const opacidad = document.querySelector('#controlOpacidad');
+const contraste = document.querySelector('#controlContraste');
+const desenfoque = document.querySelector('#controlDesenfoque');
+
+imgMeme.src = "https://images.adsttc.com/media/images/5d34/e507/284d/d109/5600/0240/newsletter/_FI.jpg?1563747560";
 
 //IR A MODO OSCURO 
 modoClaro.addEventListener('click', () => {
@@ -27,6 +35,7 @@ modoOscuro.addEventListener('click', () => {
 //ABRIR PANEL IMAGEN
 botonImagen.addEventListener('click', () => {
     aside.classList.remove('oculto')
+    panelImagen.classList.remove('oculto');
     panelMeme.classList.add('oculto');
 });
 
@@ -41,14 +50,38 @@ cerrarPanelImagen.addEventListener('click', () => {
 botonTexto.addEventListener('click', () => {
     panelMeme.classList.add('oculto');
     aside.classList.remove('oculto');
-    panelTexto.classList.remove('oculto');
     panelImagen.classList.add('oculto');
-})
+    panelTexto.classList.remove('oculto');
+   
+});
+
+//CERRAR PANEL TEXTO
+
+cerrarPanelTexto.addEventListener('click', () => {
+    panelMeme.classList.remove('oculto')
+    aside.classList.add('oculto')
+    panelTexto.classList.add('oculto')
+}); 
 
 
 
+//    FUNCIONALIDADES PANEL IMAGEN
 
+//URL
+urlImg.addEventListener('change', () => {
+    imgMeme.src = urlImg.value
+});
 
+//FILTROS
+const actualizarFiltros = () => {
+    imgMeme.style.filter = `brillo(${brillo.value}) opacidad(${opacidad.value}) contraste(${contraste.value}%) desenfoque(${desenfoque.value}px)`;
+  }
 
+  brillo.addEventListener("change", actualizarFiltros);
 
+  opacidad.addEventListener("change", actualizarFiltros);
+
+  contraste.addEventListener("change", actualizarFiltros);
+
+  desenfoque.addEventListener("change", actualizarFiltros);
 
