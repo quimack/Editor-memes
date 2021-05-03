3938c8681a2a6957d28fb26a0397a896eb746e03
quimack <75865290+quimack@users.noreply.github.com>
2021-05-02 22:31:38 -0300

    agregué las funcionalidades para subir la imagen, actualizar los filtros, y modificar el texto

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const body = document.querySelector('#body');
+//BOTONES Y PANELES
 const modoClaro = document.querySelector('#modoClaro');
 const modoOscuro = document.querySelector('#modoOscuro');
 const botonImagen = document.querySelector('#botonImagen');
@@ -8,12 +9,32 @@ const cerrarPanelImagen = document.querySelector('#cerrarPanelImagen');
 const panelImagen = document.querySelector('#panelImagen');
 const panelMeme = document.querySelector('#panelMeme');
 const aside = document.querySelector('#aside');
+//URL - IMAGEN
 const urlImg = document.querySelector('#urlImg');
 const imgMeme = document.querySelector('#imgMeme');
+//INPUTS FILTROS:
 const brillo = document.querySelector('#controlBrillo');
 const opacidad = document.querySelector('#controlOpacidad');
 const contraste = document.querySelector('#controlContraste');
 const desenfoque = document.querySelector('#controlDesenfoque');
+const grises = document.querySelector('#controlGrises');
+const sepia = document.querySelector('#controlSepia');
+const hue = document.querySelector('#controlHue');
+const saturado = document.querySelector('#controlSaturado');
+const negativo = document.querySelector('#controlNegativo');
+//DESCARGA IMAGEN
+const botonDescarga = document.getElementById('botonDescarga');
+const contenedorMeme = document.getElementById('contenedorMeme');
+//INPUTS TEXTO Y MODIFICADORES
+const topText = document.querySelector('#topText');
+const bottomText = document.querySelector('#bottomText');
+const topTextMeme = document.querySelector('#topTextMeme');
+const bottomTextMeme = document.querySelector('#bottomTextMeme');
+const fuenteTexto = document.querySelector('#fuenteTexto');
+const tamañoFuente = document.querySelector('#tamañoFuente');
+const colorTexto = document.querySelector('#colorTexto');
+const fondoTexto = document.querySelector('#fondoTexto');
+const fondoTextoTransparente = document.querySelector('#fondoTextoTransparente');
 
 imgMeme.src = "https://images.adsttc.com/media/images/5d34/e507/284d/d109/5600/0240/newsletter/_FI.jpg?1563747560";
 
@@ -74,14 +95,66 @@ urlImg.addEventListener('change', () => {
 
 //FILTROS
 const actualizarFiltros = () => {
-    imgMeme.style.filter = `brillo(${brillo.value}) opacidad(${opacidad.value}) contraste(${contraste.value}%) desenfoque(${desenfoque.value}px)`;
+    imgMeme.style.filter = `brightness(${brillo.value}) opacity(${opacidad.value}) contrast(${contraste.value}%) blur(${desenfoque.value}px)
+    grayscale(${grises.value}) sepia(${sepia.value}) hue-rotate(${hue.value}) saturate(${saturado.value}) invert(${negativo.value})`;
   }
 
   brillo.addEventListener("change", actualizarFiltros);
-
   opacidad.addEventListener("change", actualizarFiltros);
-
   contraste.addEventListener("change", actualizarFiltros);
-
   desenfoque.addEventListener("change", actualizarFiltros);
+  grises.addEventListener("change", actualizarFiltros);
+  sepia.addEventListener("change", actualizarFiltros);
+  hue.addEventListener("change", actualizarFiltros);
+  saturado.addEventListener("change", actualizarFiltros);
+  negativo.addEventListener("change", actualizarFiltros);
+
+// FUNCIONALIDADES PANEL TEXTO
+topText.addEventListener('input', () =>{
+    topTextMeme.textContent = topText.value;
+});
+
+bottomText.addEventListener('input', () =>{
+    bottomTextMeme.textContent = bottomText.value;
+});
+
+fuenteTexto.addEventListener('change', () => {
+    topTextMeme.style.fontFamily = fuenteTexto.value;
+    bottomTextMeme.style.fontFamily = fuenteTexto.value;
+});
+
+tamañoFuente.addEventListener('change', () => {
+    topTextMeme.style.fontSize = `${tamañoFuente.value}px`;
+    bottomTextMeme.style.fontSize = `${tamañoFuente.value}px`;
+});
+
+colorTexto.addEventListener('change' , () => {
+    topTextMeme.style.color = colorTexto.value;
+    bottomTextMeme.style.color = colorTexto.value;
+});
+
+fondoTexto.addEventListener('change', () => {
+        topTextMeme.style.backgroundColor = fondoTexto.value;
+        bottomTextMeme.style.backgroundColor = fondoTexto.value;
+});
+
+fondoTextoTransparente.addEventListener('input', () => {
+    topTextMeme.style.backgroundColor = "transparent";
+    bottomTextMeme.style.backgroundColor = "transparent";
+});
+
+
+
+
+
+
+
+//DESCARGA IMAGEN - NO FUNCIONA
+
+botonDescarga.addEventListener('click', () => {
+    domtoimage.toBlob(contenedorMeme)
+    .then(function (blob) {
+    window.saveAs(blob, 'meme.png');
+  });
+});
 
